fbe4c7b07af1fbd1d8053a970b86a16688bf3854
royandugu <royandugu99009@gmail.com>
2022-12-23 21:23:43 +0545

    Can know if messaged

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Dependencies
 1. discord (obviously)
 2. python-dotenv
+
+
+
+## Note
+1. Learn about Python decorators
+2. Learn about this intents thingy
--- main.py
+++ main.py
@@ -6,10 +6,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
-client=discord.Client() 
+client=discord.Client(intents=discord.Intents.default()) 
 
 @client.event
 async def on_ready():
     print(f"Logged in as {format(client)}")
+
+@client.event
+async def on_message(message):
+    print(message)
 
 client.run(TOKEN)
