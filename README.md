e9bdb4115f496c3e9d913b0d201aeb7d09b9ab82
royandugu <royandugu99009@gmail.com>
2023-01-05 22:00:09 +0545

    selenium and our endpoint url

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Dependencies
 1. discord (obviously)
 2. python-dotenv
-
+3. selenium (for web scrapping)
 
 
 ## Note
 1. Learn about this intents thingy
+2. We are not being able to access message content because of some options fix them << still we don't even need message content but if we want to add some bot interfaces>>
--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import os
 import discord
 
 from dotenv import load_dotenv 
+from selenium import webdriver
 
 load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
 client=discord.Client(intents=discord.Intents.default()) 
 
+#Loading the browser
+browser=webdriver.Edge() #using microsoft edge browser
+browser.get("https://ncit.edu.np/news")
+
 @client.event
 async def on_ready():
-    print("The bot has logged in")
+    print("The bot has logged in")    
 
 @client.event
 async def on_message(message):
@@ -21,7 +26,6 @@ async def on_message(message):
     if authorName == client.user: #Making sure the bot doesn't reply to itself 
         return 
     
-    print(conte)
     if (content.lower() == "hi" or content.lower() == "hello"):
         await message.channel.send(f"Hello {authorName}")
     elif (content.lower() == "bye"):
